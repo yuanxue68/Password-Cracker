81a9c3e676c08abd680b4ae0d967ae7387cd098c
Xue  Yuan <yuanxue68@gmail.com>
2015-03-30 15:20:52 -0400

    first commit

---
+++ FileServer.java
@@ -0,0 +1,111 @@
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException.Code;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.Watcher.Event.EventType;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class FileServer {
+    
+    String myPath = "/fileServer";
+    ZkConnector zkc;
+    Watcher watcher;
+    static String host;
+    static int port;
+    public static ArrayList<String> password=new ArrayList<String>();
+
+    public static void main(String[] args) {
+      
+        if (args.length != 2) {
+            System.out.println("Usage: java -classpath lib/zookeeper-3.3.2.jar:lib/log4j-1.2.15.jar:. Test zkServer:clientPort");
+            return;
+        }
+
+        try{
+        	host=InetAddress.getLocalHost().getHostName();
+        	port=Integer.parseInt(args[1]);
+	        BufferedReader br = new BufferedReader(new FileReader("dictionary/lowercase.rand"));
+	        String line;
+	        while ((line = br.readLine()) != null) {
+	        	password.add(line);
+	        }
+	        FileServer t = new FileServer(args[0]);
+	        
+	        t.checkpath();
+	        
+	        ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new FileServerThread(socket).start();
+            }
+        }
+        catch(Exception e){
+        	
+        }
+       
+    }
+
+    public FileServer(String hosts) {
+        zkc = new ZkConnector();
+        try {
+            zkc.connect(hosts);
+        } catch(Exception e) {
+            System.out.println("Zookeeper connect "+ e.getMessage());
+        }
+ 
+        watcher = new Watcher() { // Anonymous Watcher
+                            @Override
+                            public void process(WatchedEvent event) {
+                                handleEvent(event);
+                        
+                            } };
+    }
+    
+    private void checkpath() {
+        Stat stat = zkc.exists(myPath, watcher);
+        if (stat == null) {              // znode doesn't exist; let's try creating it
+            System.out.println("Creating " + myPath);
+            Code ret = zkc.create(
+                        myPath,         // Path of znode
+                        null,           // Data not needed.
+                        CreateMode.EPHEMERAL   // Znode type, set to EPHEMERAL.
+                        );
+            if (ret == Code.OK) System.out.println("the boss!");
+        } 
+    }
+
+    private void handleEvent(WatchedEvent event) {
+        String path = event.getPath();
+        EventType type = event.getType();
+        if(path.equalsIgnoreCase(myPath)) {
+            if (type == EventType.NodeDeleted) {
+                System.out.println(myPath + " deleted! Let's go!");       
+                checkpath(); // try to become the boss
+            }
+            if (type == EventType.NodeCreated) {
+                System.out.println(myPath + " created!");       
+                try{ Thread.sleep(5000); } catch (Exception e) {}
+                checkpath(); // re-enable the watch
+            }
+        }
+    }
+
+}
+class FileServerThread extends Thread {
+	FileServerThread(Socket socket){
+		
+	}
+	
+}
+
